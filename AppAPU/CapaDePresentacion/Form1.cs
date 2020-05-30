4f0c0cf420f990ce66aaa4bbedd03bee85bf18df
Luis Felipe Navarro Julio <lfnavarroj@hotmail.com>
2020-05-30 18:10:24 -0500

    Primera app

---
+++ AppAPU/CapaDePresentacion/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//
+using CapaLogicaDeNegocio;
+
+namespace CapaDePresentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clsMateriales objeto = new clsMateriales();
+            objeto.Cod_material = 1;
+            objeto.Nombre_material = "Cable#2";
+            objeto.Precio_material = 1250;
+
+            label1.Text= ("El material creado es " + objeto.Nombre_material +" Cuesta " +objeto.Precio_material.ToString());
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ AppAPU/CapaLogicaDeNegocio/clsMateriales.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias a las otras capas
+using CapaDeDatos;
+
+namespace CapaLogicaDeNegocio
+{
+    public class clsMateriales
+    {
+        //Atributos
+        private int _cod_material;
+        private string _nombre_material;
+        private float _precio_material;
+
+        //Propiedades
+        public int Cod_material { get => _cod_material; set => _cod_material = value; }
+        public string Nombre_material { get => _nombre_material; set => _nombre_material = value; }
+        public float Precio_material { get => _precio_material; set => _precio_material = value; }
+    }
+}
