ccf04d9f3e3ff6cc8279208be01895421595e01c
joaquincaicedon <joacocn@gmail.com>
2020-05-30 18:23:47 -0500

    Borré el trabajo inútil

--- AppAPU/CapaLogicaDeNegocio/clsMateriales.cs
+++ AppAPU/CapaLogicaDeNegocio/clsMateriales.cs
@@ -10,14 +10,6 @@ namespace CapaLogicaDeNegocio
 {
     public class clsMateriales
     {
-        //Atributos
-        private int _cod_material;
-        private string _nombre_material;
-        private float _precio_material;
-
-        //Propiedades
-        public int Cod_material { get => _cod_material; set => _cod_material = value; }
-        public string Nombre_material { get => _nombre_material; set => _nombre_material = value; }
-        public float Precio_material { get => _precio_material; set => _precio_material = value; }
+     
     }
 }
