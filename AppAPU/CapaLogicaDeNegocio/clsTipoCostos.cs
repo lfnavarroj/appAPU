5e877810adb5dca7c6d75c67c526068c674423da
Jose Maria Gil <josegilv@gmail.com>
2020-06-06 17:26:18 -0500

    Se agregó clase TipoCostos

---
+++ AppAPU/CapaLogicaDeNegocio/clsTipoCostos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class clsTipoCostos
+{
+	public clsTipoCostos()
+	{
+	private int id_tipo;
+	private string descripcion_tipo_costo;
+
+    public int Id_tipo { get => id_tipo; set => id_tipo = value; }
+    public string Descripcion_tipo_costo { get => descripcion_tipo_costo; set => descripcion_tipo_costo = value; }
+}
+}
