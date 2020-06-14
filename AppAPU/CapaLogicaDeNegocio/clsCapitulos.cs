c366f528f8007daaf457f4d0bf9e71eb23f4be4c
Jose Maria Gil <josegilv@gmail.com>
2020-06-14 18:07:04 -0500

    Se recuperan las clases del modelo E-R

---
+++ AppAPU/CapaLogicaDeNegocio/clsCapitulos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogicaDeNegocio
+{
+    class clsCapitulos
+    {
+        private string _id_capitulo;
+        private string _descripcion_capitulo;
+
+        public string Id_capitulo { get => _id_capitulo; set => _id_capitulo = value; }
+        public string Descripcion_capitulo { get => _descripcion_capitulo; set => _descripcion_capitulo = value; }
+    }
+}
---
+++ AppAPU/CapaLogicaDeNegocio/clsPerfiles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogicaDeNegocio
+{
+    class clsPerfiles
+    {
+        private string _id_perfil;
+        private string _nombre_perfil;
+
+        public string Id_perfil { get => _id_perfil; set => _id_perfil = value; }
+        public string Nombre_perfil { get => _nombre_perfil; set => _nombre_perfil = value; }
+    }
+}
